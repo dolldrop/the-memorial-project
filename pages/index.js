ec9172b9ea92789b105f7d4a9521f6d62c73bddd
dolldrop <sav4rit3@gmail.com>
2024-09-25 18:22:41 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,16 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
+    .button {
+  background-color: #04AA6D; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
 
       <Footer />
     </div>
